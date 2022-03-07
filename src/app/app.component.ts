bf3b8d93fdb564ca6e9aa0fccb819a934d5ab467
Rustam Mukhametshin <itechman@yandex.ru>
2022-03-08 02:46:18 +0500

    Revert "Dynamic component"
    This reverts commit 4ae992aaf3e6f5669900c17ec0e75dce7bf75cbc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,9 @@
-import { Component, ComponentFactoryResolver, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { DynamicComponent } from './dynamic/dynamic.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [
-    AsyncPipe,
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('dynamic', {
-    read: ViewContainerRef
-  }) private viewContainerRef!: ViewContainerRef;
-
-  private componentRef!: ComponentRef<DynamicComponent>;
-
-  constructor(
-    private readonly componentFactoryResolver: ComponentFactoryResolver
-  ) {
-  }
-
-  showDynamicComponent() {
-    this.viewContainerRef.clear();
-
-    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(DynamicComponent);
-
-    this.componentRef = this.viewContainerRef.createComponent<DynamicComponent>(componentFactory);
-  }
-
-  removeDynamicComponent() {
-    this.componentRef.destroy();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import { DynamicComponent } from './dynamic/dynamic.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DynamicComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgxSkeletonLoaderModule.forRoot({
-      animation: 'pulse'
-    }),
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
