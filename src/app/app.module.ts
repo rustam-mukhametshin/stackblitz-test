d6016d80c5d2dbee0858705cb66ab0318250d5bb
Rustam Mukhametshin <itechman@yandex.ru>
2022-03-15 10:38:12 +0500

    Revert "feat: add preview components"
    This reverts commit 748588152e5ee6bc6e7c05384385f75269f7e6c5.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AsyncPipe } from '@angular/common';
 import { CatComponent } from './views/cat/cat.component';
-import { PreviewComponent } from './views/preview/preview.component';
-import { PreviewPaginationComponent } from "./views/preview-pagination/preview-pagination.component";
-import { PreviewActionComponent } from './views/preview-action/preview-action.component';
-import { PreviewZoomComponent } from './views/preview-zoom/preview-zoom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatComponent,
-    PreviewComponent,
-    PreviewPaginationComponent,
-    PreviewActionComponent,
-    PreviewZoomComponent,
+    CatComponent
   ],
   imports: [
     BrowserModule,
